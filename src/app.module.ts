a798e5ae6337a52178d9e4dbbc9335e2a870b981
zsoon96 <pres0616@gmail.com>
2022-08-24 22:30:52 +0900

    Board 모듈 추가 후, BoardEntity 생성 / BoardStatus enum 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { BoardsModule } from './boards/boards.module';
 
 // 프로젝트 최상위 모듈
 @Module({
   // db 설정값 넣어주기 > 별도의 지정 설정값이 없으면 ormconfig.json 자동으로 찾아서 실행
   // forRoot안에 넣어준 설정은 모든 sub-module 부수적인 모듈들에 모두 적용됨
   imports: [TypeOrmModule.forRoot(),
-    UserModule],
+    UserModule,
+    BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
