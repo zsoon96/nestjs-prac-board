52ba5a1c48b43a36c2231b0cba81cc88b1b150e8
zsoon96 <pres0616@gmail.com>
2022-08-24 22:21:21 +0900

    TypeORM 모듈 설치 및 db 정보에 대한 Config 파일(ormconfig.json) 생성 후, app 모듈에 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 // 프로젝트 최상위 모듈
 @Module({
-  imports: [UserModule],
+  // db 설정값 넣어주기 > 별도의 지정 설정값이 없으면 ormconfig.json 자동으로 찾아서 실행
+  // forRoot안에 넣어준 설정은 모든 sub-module 부수적인 모듈들에 모두 적용됨
+  imports: [TypeOrmModule.forRoot(),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
