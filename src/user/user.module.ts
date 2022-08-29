92ad9b4139d4f710d6f237824c095a82edfcd5c7
zsoon96 <pres0616@gmail.com>
2022-08-30 07:06:26 +0900

    Passport 모듈 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,13 @@ import { UserService } from './user.service';
 import {CustomTypeOrmModule} from "../db/typeorm.module";
 import {UserRepository} from "./user.repository";
 import {JwtModule} from "@nestjs/jwt";
+import {PassportModule} from "@nestjs/passport";
 
 @Module({
-  imports: [ JwtModule.register({
-    secret: '서버의 시크릿 키',
+  // passport 모듈은 JWT를 이용해서 인증 처리하는 등의 과정을 훨씬 수월하게 도와주는 역할
+  imports: [ PassportModule.register({defaultStrategy:'jwt'}),
+      JwtModule.register({
+    secret: '토큰을 만들때 필요한 서버의 시크릿 키',
     signOptions: {
       expiresIn: 3600 // 1h
     }
