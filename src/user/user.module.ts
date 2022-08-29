738a43253890bda1ff5e1b3b3580b092ee9cd444
zsoon96 <pres0616@gmail.com>
2022-08-30 07:03:18 +0900

    JWT 모듈 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,16 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import {CustomTypeOrmModule} from "../db/typeorm.module";
 import {UserRepository} from "./user.repository";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  imports: [CustomTypeOrmModule.forCustomRepository([UserRepository])],
+  imports: [ JwtModule.register({
+    secret: '서버의 시크릿 키',
+    signOptions: {
+      expiresIn: 3600 // 1h
+    }
+  }),
+      CustomTypeOrmModule.forCustomRepository([UserRepository])],
   controllers: [UserController],
   providers: [UserService]
 })
