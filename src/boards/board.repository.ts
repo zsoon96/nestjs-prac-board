ba3e6aebe29c1194f77ed755e297e8819264356f
zsoon96 <pres0616@gmail.com>
2022-12-08 07:16:50 +0900

    게시글 등록 예외처리

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -3,6 +3,7 @@ import {Repository} from "typeorm";
 import {CustomRepository} from "../db/typeorm.decorator";
 import {CreateBoardDto} from "./dto/create-board.dto";
 import {BoardStatus} from "./board-status.enum";
+import { BadRequestException } from "@nestjs/common";
 
 // db 관련 로직 처리를 위한 레포지토리
 @CustomRepository(Board)
@@ -11,14 +12,20 @@ export class BoardRepository extends Repository<Board> {
     async createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> { {
         const { title, description } = createBoardDto;
 
-        const board = this.create({
-            title,
-            description,
-            status: BoardStatus.PUBLIC,
-            user
-        })
+        try {
+            const board = this.create({
+                title,
+                description,
+                status: BoardStatus.PUBLIC,
+                user
+            })
 
-        await this.save(board);
-        return board;
+            await this.save(board);
+            return board;
+        }
+        catch (err) {
+            throw new BadRequestException('입력값을 확인해주세요.')
+        }
+
     }}
 }
