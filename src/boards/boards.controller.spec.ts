18ff41ef0ae96d4f805e8a4fe366f279b2148f31
zsoon96 <pres0616@gmail.com>
2022-12-12 07:27:05 +0900

    단위 테스트 - controller 게시글 목록조회 통신

--- src/boards/boards.controller.spec.ts
+++ src/boards/boards.controller.spec.ts
@@ -4,6 +4,8 @@ import {Test, TestingModule} from "@nestjs/testing";
 import {PassportModule} from "@nestjs/passport";
 import {JwtModule} from "@nestjs/jwt";
 import {BoardStatus} from "./board-status.enum";
+import {BoardRepository} from "./board.repository";
+import {BadRequestException, NotFoundException} from "@nestjs/common";
 
 const mockService = () => ({
     getAllBoard: jest.fn(),
@@ -13,11 +15,17 @@ const mockService = () => ({
     deleteBoard: jest.fn()
 })
 
+const mockCustomRepository = () => ({
+    getAllBoard: jest.fn(),
+})
+
 type MockService<T=any> = Partial<Record<keyof T, jest.Mock>>
+type MockCustomRepository = Partial<Record<keyof BoardRepository, jest.Mock>>
 
 describe('BoardsController', () => {
     let controller:BoardsController
     let service:MockService<BoardsService>
+    let repository:MockCustomRepository
 
     beforeEach( async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -35,11 +43,16 @@ describe('BoardsController', () => {
                     provide: BoardsService,
                     useValue: mockService()
                 },
+                {
+                    provide: BoardRepository,
+                    useValue: mockCustomRepository()
+                },
             ]
         }).compile()
 
         controller = module.get(BoardsController)
         service = module.get(BoardsService)
+        repository = module.get(BoardRepository)
     })
 
     describe('POST - createBoard', () => {
@@ -69,6 +82,51 @@ describe('BoardsController', () => {
             expect(result).toBeDefined()
             expect(result).toEqual(mockBoard)
             expect(service.createBoard).toBeCalled()
+        })
+    })
+
+    describe('GET - getAllBoard', () => {
+        it('게시글 목록조회 통신 - 정상 조회', async () => {
+            const mockBoardList = [
+                {
+                    id: 1,
+                    title: '제목1',
+                    description: '내용1',
+                    status: BoardStatus.PUBLIC,
+                    user: {
+                        id: 1,
+                        name: 'user1'
+                    }
+                },
+                {
+                    id: 2,
+                    title: '제목2',
+                    description: '내용2',
+                    status: BoardStatus.PUBLIC,
+                    user: {
+                        id: 2,
+                        name: 'user2'
+                    }
+                },
+            ]
+            service.getAllBoard.mockResolvedValue(mockBoardList)
+
+            const result = await controller.getAllBoard()
+
+            expect(result).toBeDefined()
+            expect(result).toEqual(mockBoardList)
+            expect(service.getAllBoard).toBeCalled()
+        })
+
+        it('게시글 목록조회 통신 - 조회 실패', async () => {
+            service.getAllBoard.mockRejectedValue(new NotFoundException())
+
+            try {
+                const result = await controller.getAllBoard()
+                expect(result).toBeDefined()
+            } catch (err) {
+                expect(err).toBeInstanceOf(NotFoundException)
+            }
         })
     })
 })
